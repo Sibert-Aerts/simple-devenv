ef012b13ae358caef13f7a1bd6e4ac59fbe7b74e
Vincent Baggerman <vincent.baggerman@gmail.com>
2024-10-11 10:50:56 +0200

    fix

--- create.sh
+++ create.sh
@@ -80,7 +80,7 @@ replace_placeholders() {
         -e "s|\$DESIGN_THEMES_PATH|$DESIGN_THEMES_PATH|g" \
         -e "s|\$CUSTOM_ADDONS_PATH|$CUSTOM_ADDONS_PATH|g" \
         -e "s|\$USER|$USER|g" \
-        -e "s/\$PROJECT_NAME|$PROJECT_NAME|g"
+        -e "s|\$PROJECT_NAME|$PROJECT_NAME|g"
 }
 
 # Create run_odoo_debug.sh script
